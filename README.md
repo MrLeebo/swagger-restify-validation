36bcb1a5b9e237abeb048310b0a2abd9907e54e3
Jeremy Liberman <jliberman@strenuus.com>
2015-02-04 18:41:16 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # swagger-restify-validation
+============================
 A middleware extension for swagger-tools that integrates with node-restify-validations
+
+## Install
+```
+npm install swagger-restify-validation --save
+```
+
+## Usage
+
+Require swagger-restify validation,
+
+```
+var restifyValidation = require('swagger-restify-validation');
+```
+
+Then include the middleware in your initializeMiddleware callback.
+```
+swaggerTools.initializeMiddleware(swaggerDoc, function(middleware) {
+  app.use(middleware.swaggerMetadata());
+  app.use(restifyValidation());
+
+  // snip...
+});
+```
---
+++ index.js
@@ -0,0 +1,27 @@
+var _ = require('lodash');
+var restifyValidation = require('node-restify-validation');
+
+var defaultOptions = {
+  errorsAsArray: true,
+  errorHandler: true,
+  forbidUndefinedVariables: false
+};
+
+module.exports = function(options) {
+  options = _.extend(defaultOptions, options);
+
+  return function(req, res, next) {
+
+    var validationModel = req.swagger && req.swagger.operation ? req.swagger.operation['x-restify-validation'] : undefined;
+    if (!validationModel) {
+      return next();
+    }
+
+    var errors = restifyValidation.validation.process(validationModel, req, options);
+    if (errors && (options.errorsAsArray && errors.length > 0) || (!options.errorsAsArray && _.keys(errors).length > 0)) {
+      return restifyValidation.error.handle(errors, req, res, options, next);
+    }
+
+    next();
+  };
+};
